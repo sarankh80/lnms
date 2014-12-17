b3d606ff9e5781f1df3e7765589740769a6c7497
thekhmerkravsrok@gmail.com <thekhmerkravsrok@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2014-12-17 02:58:56 +0000

    upload some module to server

---
+++ trunk/application/modules/payroll/forms/FrmSalary.php
@@ -0,0 +1,52 @@
+<?php
+
+class Payroll_Form_FrmSalary extends Zend_Dojo_Form
+{
+
+    public function frmaddSalary()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    	
+    	$staff_name=new Zend_Dojo_Form_Element_ValidationTextBox('staff_name');
+    	$staff_name->setAttribs(array(
+    			'dojoType'=>'dijit.form.ValidationTextBox',
+    			'required'=>true
+    			));
+    	
+    	$Sex=new Zend_Dojo_Form_Element_FilteringSelect('sex');
+    	$Sex->setAttribs(array(
+    			'dojoType'=>'dijit.form.FilteringSelect'));
+    	$Sex->setMultiOptions(array(""));
+    	
+    	$position=new Zend_Dojo_Form_Element_TextBox('position');
+    	$position->setAttribs(array(
+    			'dojoType'=>'dijit.form.TextBox'));
+    	
+    	$Basic_salary=new Zend_Dojo_Form_Element_NumberTextBox('basic_salary');
+    	$Basic_salary->setAttribs(array(
+    			'dojoType'=>'dijit.form.NumberTextBox',
+    			'required'=>true));
+    	
+    	
+    	
+    	
+    	$for_manth=new Zend_Dojo_Form_Element_DateTextBox('for_manth');
+    	$for_manth->setAttribs(array(
+    			'dojoType'=>'dijit.form.DateTextBox'));
+    	$for_manth->setValue(date('Y-m-d'));
+    	
+    	
+    	$Start_day=new Zend_Dojo_Form_Element_DateTextBox('start_day');
+    	$Start_day->setAttribs(array(
+    			'dojoType'=>'dijit.form.DateTextBox'));
+    	$Start_day->setValue(date('Y-m-d'));
+    	
+    	
+    	
+		$this->addElements(array($staff_name,$Sex,$position,$Basic_salary,$for_manth,$Start_day));
+		return $this;
+    }
+
+
+}
+
---
+++ trunk/application/modules/tellerandexchange/controllers/indexController.php
@@ -0,0 +1,70 @@
+<?php
+class tellerandexchange_indexController extends Zend_Controller_Action {
+	private $activelist = array('មិនប្រើ​ប្រាស់', 'ប្រើ​ប្រាស់');
+    public function init()
+    {    	
+     /* Initialize action controller here */
+    	header('content-type: text/html; charset=utf8');
+    	defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
+	}
+	private $sex=array(1=>'M',2=>'F');
+	public function indexAction(){
+	
+    }
+  function addAction()
+  {
+		if($this->getRequest()->isPost()){
+			$_data = $this->getRequest()->getPost();
+			try {
+				$_dbmodel = new Loan_Model_DbTable_DbLoanIL();
+				$_dbmodel->addNewLoanIL($_data);
+// 				Application_Form_FrmMessage::Sucessfull("INSERT_SUCCESS","/loan/index/index");
+			}catch (Exception $e) {
+				echo $err =$e->getMessage();exit();
+				Application_Form_FrmMessage::message("INSERT_FAIL");
+				$err =$e->getMessage();
+				Application_Model_DbTable_DbUserLog::writeMessageError($err);
+			}
+		}
+		$frm = new Loan_Form_FrmLoan();
+		$frm_loan=$frm->FrmAddLoan();
+		Application_Model_Decorator::removeAllDecorator($frm_loan);
+		$this->view->frm_loan = $frm_loan;
+		$frmpopup = new Application_Form_FrmPopupGlobal();
+		$this->view->frmpupopclient = $frmpopup->frmPopupClient();
+		$this->view->frmPopupCO = $frmpopup->frmPopupCO();
+		$this->view->frmPopupZone = $frmpopup->frmPopupZone();
+		$this->view->frmPopupCommune = $frmpopup->frmPopupCommune();
+		$this->view->frmPopupDistrict = $frmpopup->frmPopupDistrict();
+		$this->view->frmPopupVillage = $frmpopup->frmPopupVillage();
+	}	
+	public function addloanAction(){
+		if($this->getRequest()->isPost()){
+			$data=$this->getRequest()->getPost();
+			$db = new Loan_Model_DbTable_DbLoan();
+			$id = $db->addNewLoanGroup($data);
+			$suc = array('sms'=>'ប្រាក់ឥណទានត្រូវបានបញ្ចូលដោយជោគជ័យ !');
+			print_r(Zend_Json::encode($suc));
+			exit();
+		}
+	}
+	function addAddAction()
+	{
+		if($this->getRequest()->isPost()){
+		}
+		$frm = new Loan_Form_FrmLoan();
+		$frm_loan=$frm->FrmAddLoan();
+		Application_Model_Decorator::removeAllDecorator($frm_loan);
+		$this->view->frm_loan = $frm_loan;
+	}
+	public function testAction(){
+		$start = '2014-01-01';
+		$db = new Application_Model_DbTable_DbGlobal();
+		$rs = $db->checkHolidayExist(1,$start);
+		echo $rs;
+// 		print_r($rs);
+// 		exit();
+		
+	}
+}
+
