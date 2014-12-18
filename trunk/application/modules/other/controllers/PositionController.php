a04fe8e75e786dca7fb328b38374dbfd08b7618a
Kysyheng@gmail.com <Kysyheng@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2014-12-18 08:02:12 +0000

    update position to service

--- trunk/application/modules/other/controllers/PositionController.php
+++ trunk/application/modules/other/controllers/PositionController.php
@@ -39,115 +39,57 @@ class Other_PositionController extends Zend_Controller_Action {
 		Application_Model_Decorator::removeAllDecorator($frm);
 		$this->view->frm_search = $frm;
 	}
-	public function settingAction(){
-	try{
-		$db_dept=new Global_Model_DbTable_DbDept();
-		if($this->getRequest()->isPost()){
-			$_data=$this->getRequest()->getPost();
-			$search = array(
-					'title' => $_data['title'],
-					'status' => $_data['status_search']);
-			$limit = $dept_session->limit;
-		}
-		else{
-			$search='';
-		}
-		 $_db = new Global_Model_DbTable_DbSetting();
-		 $rs_rows = $_db->getAllSetting($search);
-			 
-		 $list = new Application_Form_Frmtable();
-		 $collumns = array("KEY_VALUE","KEY_VALUE","BY_USER");
-		 $link=array(
-					'module'=>'global','controller'=>'index','action'=>'setting-edit',
-			);
-			$this->view->list=$list->getCheckList(0, $collumns, $rs_rows,array('keyvalue'=>$link));
-			    	
-    	}catch (Exception $e){
-    		Application_Form_FrmMessage::message("Application Error");
-			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
-    	}
-	    	$frm = new Global_Form_FrmSearchMajor();
-	    	$frm = $frm->FrmSetting();
-	    	Application_Model_Decorator::removeAllDecorator($frm);
-	    	$this->view->frm_search = $frm;
-	}
-	public function settingEditAction(){
-		$frm = new Global_Form_FrmSearchMajor();
-		$_model = new Global_Model_DbTable_DbSetting();
-		if($this->getRequest()->isPost()){
-			try{
-				$_data = $this->getRequest()->getPost();
-				$_model->AddNewSetting($_data);
-				Application_Form_FrmMessage::Sucessfull("/global/index/setting","ការបញ្ជូលដោយជោយជ័យ");
-			}catch (Exception $e){
-				Application_Form_FrmMessage::message("ការបញ្ជូលបរាជ័យ");
-				Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
-			}
-		}
-		$id = $this->getRequest()->getParam("id");
-		$data=null;
-		if(!empty($id)){
-			$data=$_model->getSettingById($id);
-			$frm = $frm->FrmAddSetting($data);
-			Application_Model_Decorator::removeAllDecorator($frm);
-			$this->view->frm_setting = $frm;
-			
-		}else{
-			Application_Form_FrmMessage::getUrl("/global/index/setting");
-		}
-	}
-   function addAction(){
-   	if($this->getRequest()->isPost()){
-   		$_data = $this->getRequest()->getPost();
-   		try{
-   			$db_co = new Other_Model_DbTable_DbCreditOfficer();
-   			$db_co->addCreditOfficer($_data);
-   			Application_Form_FrmMessage::message("ការ​បញ្ចូល​ជោគ​ជ័យ !");
-   		}catch(Exception $e){
-   			Application_Form_FrmMessage::message("ការ​បញ្ចូល​មិន​ជោគ​ជ័យ");
-   			$err =$e->getMessage();
-   			Application_Model_DbTable_DbUserLog::writeMessageError($err);
-   		}
-   	}
-   	$frm = new Other_Form_FrmCO();
-   	$frm_co=$frm->FrmAddCO();
-   	Application_Model_Decorator::removeAllDecorator($frm_co);
-   	$this->view->frm_co = $frm_co;
+//syheng
+ function addAction(){
+	  if($this->getRequest()->isPost()){
+	  	$_data = $this->getRequest()->getPost();
+	  	try {
+	  			$db = new Other_Model_DbTable_DbMyPosition();
+	  	        $db->addPosition($_data);
+	  	        Application_Form_FrmMessage::message("ការ​បញ្ចូល​ជោគ​ជ័យ !");
+	  	}catch(Exception $e){
+	   			Application_Form_FrmMessage::message("ការ​បញ្ចូល​មិន​ជោគ​ជ័យ");
+	   			$err =$e->getMessage();
+	   			Application_Model_DbTable_DbUserLog::writeMessageError($err);
+	   		}
+	  }
+	   //catch id form select table
+		   	$frm = new Other_Form_FrmPosition();
+		   	$frm_co=$frm->FrmAddPosition();
+		   	Application_Model_Decorator::removeAllDecorator($frm_co);
+		   	$this->view->frm_position = $frm_co;
    }
+   function getpositionAction(){
+   	
+			   	 $db=new Other_Model_DbTable_DbMyPosition();
+			     $rows=$db->getallPosition();
+   	             $this->view->list=$rows;
+   }
+  
+   //end
    function editAction(){
-   	$db_co = new Other_Model_DbTable_DbCreditOfficer();
-   	if($this->getRequest()->isPost()){
-   		$_data = $this->getRequest()->getPost();
-   		try{
-   			$db_co->addCreditOfficer($_data);
-   			Application_Form_FrmMessage::Sucessfull("ការ​បញ្ចូល​ជោគ​ជ័យ !",'/other/co');
-   		}catch(Exception $e){
-   			Application_Form_FrmMessage::message("ការ​បញ្ចូល​មិន​ជោគ​ជ័យ");
-   			$err =$e->getMessage();
-   			Application_Model_DbTable_DbUserLog::writeMessageError($err);
-   		}
-   	}
-   	$id = $this->getRequest()->getParam("id");
-   	$row = $db_co->getCOById($id);
-   	if(empty($row)){
-   		$this->_redirect('other/co');
-   	}
-   	$frm = new Other_Form_FrmCO();
-   	$frm_co=$frm->FrmAddCO($row);
-   	Application_Model_Decorator::removeAllDecorator($frm_co);
-   	$this->view->frm_co = $frm_co;
-   }
-   public function addNewcoAction(){
-   	if($this->getRequest()->isPost()){
-   		$data = $this->getRequest()->getPost();
-   		$data['status']=1;
-   		$data['co_id']='';
-   		$data['name_kh']='';
-   		$db_co = new Other_Model_DbTable_DbCreditOfficer();
-   		$id = $db_co->addCreditOfficer($data);
-   		print_r(Zend_Json::encode($id));
-   		exit();
-   	}
+    $db = new Other_Model_DbTable_DbMyPosition();
+	   	if($this->getRequest()->isPost()){
+	   		$_data = $this->getRequest()->getPost();
+	   		try{
+	   			$db->upDatePosition($_data);
+	   			Application_Form_FrmMessage::Sucessfull("ការ​បញ្ចូល​ជោគ​ជ័យ !",'/other/position');
+	   		}catch(Exception $e){
+	   			Application_Form_FrmMessage::message("ការ​បញ្ចូល​មិន​ជោគ​ជ័យ");
+	   			$err =$e->getMessage();
+	   			Application_Model_DbTable_DbUserLog::writeMessageError($err);
+	   		}
+	   	}
+	   	$id = $this->getRequest()->getParam("id");//ចាប់id from ln_position ;
+	   	$row = $db->getPositionById($id);
+	   	if(empty($row)){
+	   		$this->_redirect('other/co');
+	   	}
+        $frm = new Other_Form_FrmPosition();
+		$frm_co=$frm->FrmAddPosition($row);
+		Application_Model_Decorator::removeAllDecorator($frm_co);
+		$this->view->frm_position = $frm_co;
+		   	
    }
 }
 
---
+++ trunk/application/modules/other/models/DbTable/DbMyPosition.php
@@ -0,0 +1,87 @@
+<?php
+
+class Other_Model_DbTable_DbMyPosition extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'ln_position';
+    public function getUserId(){
+    	$session_user=new Zend_Session_Namespace('auth');
+    	return $session_user->user_id;
+    
+    }
+    
+    function addPosition($_data){
+    	$arr = array(
+    			'position_en'=>$_data['position_en'],
+    			'position_kh'=>$_data['position_kh'],
+    			'status'=>$_data['status'],
+    			'date'=>date('Y-m-d'),
+    			'displayby'=>$_data['display'],
+    			'user_id'=>$this->getUserId(),
+    			);
+    	$this->insert($arr);//insert data
+    }
+    function upDatePosition($_data){
+    	$arr = array(
+    			'position_en'=>$_data['position_en'],
+    			'position_kh'=>$_data['position_kh'],
+    			'status'=>$_data['status'],
+    			'date'=>date('Y-m-d'),
+    			'displayby'=>$_data['display'],
+    			'user_id'=>$this->getUserId(),
+    	);
+    	$where = " id = ".$_data['id'];
+    	$this->update($arr, $where);//insert data
+    }
+    public function getPositionById($id){
+    	$db = $this->getAdapter();
+    	$sql = "SELECT * FROM $this->_name WHERE id = ".$db->quote($id);
+    	$sql.=" LIMIT 1 ";
+    	$row=$db->fetchRow($sql);
+    	return $row;
+    }
+    function getallPosition(){
+    	  $db = $this->getAdapter();
+          $sql="SELECT id,position_en,position_kh,date,status,user_id,displayby  
+                FROM ln_position ORDER BY id";
+          return $db->fetchAll($sql);
+    	
+    }
+    function getPById($id){
+    	$db = $this->getAdapter();
+    	$sql="SELECT id,position_en,position_kh,date,status,user_id,displayby
+    	FROM ln_position where id = $id";
+    	return $db->fetchRow($sql);   	 
+    }
+//     function getAllBranch($search=null){
+//     	$db = $this->getAdapter();
+//     	$sql = "SELECT br_id,branch_namekh,branch_nameen,branch_address,branch_code,branch_tel,fax,other,status,display FROM 
+//     	$this->_name ";
+//     	$where = ' WHERE branch_namekh!="" AND branch_nameen !="" ';
+    
+//     	if($search['status']>-1){
+//     		$where.= " AND status = ".$search['status'];
+//     	}
+//     	if(!empty($search['adv_search'])){
+//     		$s_where = array();
+//     		$search = ($search['adv_search']);
+//     		$s_where[] = " co_code LIKE '%{$search}%'";
+//     		$s_where[] = "co_khname LIKE '%{$search}%'";
+//     		$s_where[] = " co_firstname LIKE '%{$search}%'";
+//     		$s_where[] = "co_lastname LIKE '%{$search}%'";
+//     		$s_where[] = " tel LIKE '%{$search}%'";
+//     		$s_where[] = "email LIKE '%{$search}%'";
+//     		$s_where[] = "address LIKE '%{$search}%'";
+//     		$where .=' AND ('.implode(' OR ',$s_where).')';
+//     	}
+    
+    	// 		echo $sql.$where;
+//     	return $db->fetchAll($sql.$where);
+//     }
+
+   
+    
+	
+   
+}
+
