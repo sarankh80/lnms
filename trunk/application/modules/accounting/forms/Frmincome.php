b4448137f640d882adb792763d3aa09bec9c3157
phanetnan@gmail.com <phanetnan@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2014-12-18 07:54:03 +0000

    add income n expense

--- trunk/application/modules/accounting/forms/Frmincome.php
+++ trunk/application/modules/accounting/forms/Frmincome.php
@@ -7,29 +7,55 @@ Class Accounting_Form_Frmincome extends Zend_Dojo_Form {
 	}
 	public function FrmIncome($data=null){
 		
-		$income_name = new Zend_Dojo_Form_Element_FilteringSelect('Income_name');
-		$income_name->setAttribs(array(
+		$account_id = new Zend_Dojo_Form_Element_TextBox('account_id');
+		$account_id->setAttribs(array(
+				'dojoType'=>'dijit.form.TextBox',
+				'class'=>'fullside'
+				));
+		$for_date = new Zend_Dojo_Form_Element_FilteringSelect('for_date');
+		$for_date->setAttribs(array(
 				'dojoType'=>'dijit.form.FilteringSelect',
 				'class'=>'fullside'
 				));
+		$opt_month="";
+		for($i=1;$i<=12;$i++){
+			$opt_month[$i]=$i;
+		}
+		$for_date->setMultiOptions($opt_month);
 		
 		$_Date = new Zend_Dojo_Form_Element_DateTextBox('Date');
 		$_Date->setAttribs(array(
-				'dojoType'=>'dijit.form.FilteringSelect',
+				'dojoType'=>'dijit.form.DateTextBox',
 				'required'=>true,
 				'class'=>'fullside'
 		));
+		$_Date->setValue(date('Y-m-d'));
+		
+		$_branch_id = new Zend_Dojo_Form_Element_FilteringSelect('branch_id');
+		$_branch_id->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'class'=>'fullside',
+				'required' =>'true'
+			  
+		));
+		$options= array(1=>"សាខា កណ្តាល",2=>"សាខា ទី១");
+		$_branch_id->setMultiOptions($options);
 		
 		$_stutas = new Zend_Dojo_Form_Element_FilteringSelect('Stutas');
 		$_stutas ->setAttribs(array(
 				'dojoType'=>'dijit.form.FilteringSelect',
 				'class'=>'fullside',	
 		));
+		$options= array(1=>"ប្រើប្រាស់",2=>"មិនប្រើប្រាស់");
+		$_stutas->setMultiOptions($options);
+		
+		
 		$total_amount=new Zend_Dojo_Form_Element_NumberTextBox('total_amount');
 		$total_amount->setAttribs(array(
 				'dojoType'=>'dijit.form.NumberTextBox',
 				'class'=>'fullside',
 				));
+		
 		
 		$_Description = new Zend_Dojo_Form_Element_Textarea('Description');
 		$_Description ->setAttribs(array(
@@ -38,8 +64,21 @@ Class Accounting_Form_Frmincome extends Zend_Dojo_Form {
 				'style'=>'width:98%',
 		));
 		
-		$this->addElements(array($income_name,$_Date ,$_stutas,$total_amount,$_Description));
-		return $this;
+		$id = new Zend_Form_Element_Hidden("id");
 		
-	}	
+			if($data!=null){
+					
+				$_branch_id->setValue($data['branch_id']);
+				$account_id->setValue($data['account_id']);
+				$total_amount->setValue($data['total_amount']);
+				$for_date->setValue($data['fordate']);
+				$_Description->setValue($data['disc']);
+				$_Date->setValue($data['date']);
+				$_stutas->setValue($data['status']);
+				$id->setValue($data['id']);
+		   }	
+		$this->addElements(array($account_id,$_Date ,$_stutas,$total_amount,
+				$_Description,	$_branch_id,$for_date,$id));
+		return $this;
+	}
 }
---
+++ trunk/application/modules/accounting/models/DbTable/DbExpense.php
@@ -0,0 +1,47 @@
+<?php
+class Accounting_Model_DbTable_DbExpense extends Zend_Db_Table_Abstract
+{
+	protected $_name = 'ln_income_expense';
+	function addexpense($data){
+		$data = array(
+				'branch_id'=>$data['branch_id'],
+				'account_id'=>$data['account_id'],
+				'total_amount'=>$data['total_amount'],
+				'fordate'=>$data['for_date'],
+				'tran_type'=>1,
+				'disc'=>$data['Description'],
+				'date'=>$data['Date'],
+				'status'=>$data['Stutas'],
+		);
+		$this->insert($data);
+
+}
+function updatexpense($data){
+	$arr = array(
+				'branch_id'=>$data['branch_id'],
+				'account_id'=>$data['account_id'],
+				'total_amount'=>$data['total_amount'],
+				'fordate'=>$data['for_date'],
+				'disc'=>$data['Description'],
+				'status'=>$data['Stutas'],
+			   
+				
+		);
+	$where=" id = ".$data['id'];
+	$this->update($arr, $where);
+}
+function getexpensebyid($id){
+	$db = $this->getAdapter();
+	$sql=" SELECT id,branch_id,account_id,total_amount,fordate,disc,date,status FROM $this->_name where id=$id ";
+	return $db->fetchRow($sql);
+}
+
+function getAllExpense($search=null){
+	$db = $this->getAdapter();
+	$sql=" SELECT id,branch_id,account_id,total_amount,fordate,disc,date,status FROM $this->_name ";
+	return $db->fetchAll($sql);
+}
+
+
+
+}
