990000d260c5b6b29ba74f50e7514b650ed5dd72
sovon.khom@gmail.com <sovon.khom@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2014-12-23 08:35:57 +0000

    have update co and permission to server

---
+++ trunk/application/modules/payroll/forms/FrmPermission.php
@@ -0,0 +1,113 @@
+<?php
+
+class Payroll_Form_FrmPermission extends Zend_Dojo_Form
+{
+
+    public function frmPermission()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    	$opt_employee=array(1=>'ធុង​ កក្កដា',2=>'ថុន សម្បត្តិ',3=>'មក ចន្នី');
+    	$employee=new Zend_Dojo_Form_Element_FilteringSelect('employee');
+    	$employee->setAttribs(array(
+    			'dojoType'=>'dijit.form.FilteringSelect',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	$employee->setMultiOptions($opt_employee);
+    	
+    	$approve_by=new Zend_Dojo_Form_Element_FilteringSelect('approve_by');
+    	$approve_by->setAttribs(array(
+    			'dojoType'=>'dijit.form.FilteringSelect',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	$approve_by->setMultiOptions(array("ប្រធានសាខា","ប្រធាតំបន់"));
+    	
+    	$request_date=new Zend_Dojo_Form_Element_DateTextBox('request_date');
+    	$request_date->setAttribs(array(
+    			'dojoType'=>'dijit.form.DateTextBox',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	$opt_type=array(1=>'ឈឺ',2=>'អាពាហ៍ពិពាហ៍',3=>'ពិធីបុណ្យ',4=>'ចុះខេត្ត');
+    	$type=new Zend_Dojo_Form_Element_FilteringSelect('type');
+    	$type->setAttribs(array(
+    			'dojoType'=>'dijit.form.FilteringSelect',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	
+    	$type->setMultiOptions($opt_type);
+    	$from_date=new Zend_Dojo_Form_Element_DateTextBox('from_date');
+    	$from_date->setAttribs(array(
+    			'dojoType'=>'dijit.form.DateTextBox',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	$from_date->setValue(date('Y-m-d'));
+    	
+    	
+    	$to_date=new Zend_Dojo_Form_Element_DateTextBox('to_date');
+    	$to_date->setAttribs(array(
+    			'dojoType'=>'dijit.form.DateTextBox',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	$to_date->setValue(date('Y-m-d'));
+    	
+    	$time=new Zend_Dojo_Form_Element_TextBox('time');
+    	$time->setAttribs(array(
+    			'dojotype'=>'dijit.form.TextBox',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	$time->setValue('00:00');
+    	$all_day=new Zend_Dojo_Form_Element_CheckBox('all_day');
+    	$all_day->setAttribs(array(
+    			'dojotype'=>'dijit.form.CheckBox',
+    			'required'=>true,
+    	));
+    	
+    	$paid_leave=new Zend_Dojo_Form_Element_CheckBox('paid_leave');
+    	$paid_leave->setAttribs(array(
+    			'dojotype'=>'dijit.form.CheckBox',
+    			'required'=>true,
+    	));
+    	
+    	$every_day=new Zend_Dojo_Form_Element_CheckBox('every_day');
+    	$every_day->setAttribs(array(
+    			'dojotype'=>'dijit.form.CheckBox',
+    			'required'=>true,
+    	));
+//     	$opt=array(1=>'All Day',2=>'Paid Leave',3=>'Every Day');
+//     	$choose->setMultiOptions($opt);
+    	
+    	$reason=new Zend_Dojo_Form_Element_TextBox('reason');
+    	$reason->setAttribs(array(
+    			'dojotype'=>'dijit.form.TextBox',
+    			'required'=>true,
+    			'class'=>'fullside'
+    			));
+    	
+    	$branch_id = new Zend_Dojo_Form_Element_FilteringSelect('branch_id');
+		$branch_id->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'class'=>'fullside',
+				'required' =>'true'
+		));
+		$db = new Application_Model_DbTable_DbGlobal();
+		$rows = $db->getAllBranchName();
+		$options='';
+		if(!empty($rows))foreach($rows AS $row){
+			$options[$row['br_id']]=$row['branch_namekh'];
+		}
+		$branch_id->setMultiOptions($options);
+		
+		$this->addElements(array($employee,$approve_by,$request_date,$type,$from_date,$to_date,$time,
+				$all_day,$reason,$paid_leave,$every_day,$branch_id));
+		return $this;
+    }
+
+
+}
+
---
+++ trunk/application/modules/payroll/models/DbTable/DbPermission.php
@@ -0,0 +1,79 @@
+<?php
+
+class Payroll_Model_DbTable_DbPermission extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'ln_permission';
+    public function getUserId(){
+    	$session_user=new Zend_Session_Namespace('auth');
+    	return $session_user->user_id;
+    }
+	public function addPermission($_data){
+		$_arr=array(
+				'employee_id'	=> $_data['employee'],
+				'branch_id'		=>$_data['branch_id'],
+				'approve_by'	=> $_data['approve_by'],
+				'request_date'  => $_data['request_date'],
+				'type'	    	=> $_data['type'],
+				'from_date'   	=> $_data['from_date'],
+				'to_date'		=> $_data['to_date'],
+				'time'		    => $_data['time'],
+				'all_day'     	=> !empty($_data['all_day'])?$_data['all_day']:0,
+				'paid_leave'    => !empty($_data['paid_leave'])?$_data['paid_leave']:0,
+				'every_day'     => !empty($_data['every_day'])?$_data['every_day']:0,
+				'reason'		=> $_data['reason'],
+				'date'			=> date('Y-m-d'),
+				'user_id'		=> $this->getUserId()
+		);
+		$this->insert($_arr);
+// 		if(!empty($_data['id'])){
+// 			$where = 'co_id = '.$_data['id'];
+// 			return  $this->update($_arr, $where);
+// 		}else{
+// 			return  $this->insert($_arr);
+// 		}
+		
+	}
+	public function getCOById($id){
+		$db = $this->getAdapter();
+		$sql = "SELECT * FROM $this->_name WHERE co_id = ".$db->quote($id);
+		$sql.=" LIMIT 1 ";
+		$row=$db->fetchRow($sql);
+		return $row;
+	}
+	function getAllPermission($search=null){
+		$db = $this->getAdapter();
+		$sql = "SELECT id,
+		(SELECT co_khname FROM ln_co WHERE co_id = employee_id limit 1 ) AS staff_name,
+		(SELECT branch_namekh FROM ln_branch WHERE br_id = branch_id) AS branch_name,
+		(SELECT co_khname FROM ln_co WHERE co_id = approve_by limit 1 ) AS approve_by,
+		 request_date,
+		 (SELECT name_kh FROM ln_view WHERE TYPE=7 limit 1) AS type,
+		 from_date,to_date,time,reason,
+		(SELECT user_name FROM rms_users WHERE id = user_id limit 1 ) AS user_id,
+		 date, status
+		 FROM $this->_name ";
+		$row=$db->fetchAll($sql);
+		return $row;
+		//$where = ' WHERE co_khname!="" ';
+		
+// 		if($search['status']>-1){
+// 			$where.= " AND status = ".$search['status'];
+// 		}
+// 		if(!empty($search['adv_search'])){
+// 			$s_where = array();
+// 			$search = ($search['adv_search']);
+// 			$s_where[] = " co_code LIKE '%{$search}%'";
+// 			$s_where[] = "co_khname LIKE '%{$search}%'";
+// 			$s_where[] = " co_firstname LIKE '%{$search}%'";
+// 			$s_where[] = "co_lastname LIKE '%{$search}%'";
+// 			$s_where[] = " tel LIKE '%{$search}%'";
+// 			$s_where[] = "email LIKE '%{$search}%'";
+// 			$s_where[] = "address LIKE '%{$search}%'";
+// 			$where .=' AND ('.implode(' OR ',$s_where).')';
+// 		}
+		
+// 		echo $sql.$where;
+// 		return $db->fetchAll($sql.$where);	
+	}	
+}
+
