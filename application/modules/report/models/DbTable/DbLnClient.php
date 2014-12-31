3a12308d5e1e1b2bb51b0651c0fc3b3311ca607e
phanetnan <phanetnan@gmail.com>
2014-12-31 08:17:53 +0000

    report

--- application/modules/report/models/DbTable/DbLnClient.php
+++ application/modules/report/models/DbTable/DbLnClient.php
@@ -6,10 +6,15 @@ class Report_Model_DbTable_DbLnClient extends Zend_Db_Table_Abstract
 
     public function getAllLnClient(){
     	 $db = $this->getAdapter();
-          $sql="SELECT client_number,name_kh,name_en,sex,status,(SELECT branch_namekh FROM ln_branch WHERE br_id =branch_id limit 1) AS branch_name
-          ,pro_id,dis_id,com_id,village_id,spouse_name,phone FROM ln_client ORDER BY client_id";
+          $sql="SELECT client_number,name_kh,name_en,(SELECT name_kh FROM ln_view WHERE TYPE = 11 AND key_code=sex ) AS sex,status,
+          (SELECT branch_namekh FROM ln_branch WHERE br_id =branch_id limit 1) AS branch_name
+          ,(SELECT province_kh_name FROM ln_province WHERE province_id=pro_id) As pro_id
+          ,(SELECT district_name FROM ln_district WHERE dis_id=dis_id limit 1)As dis_id
+          ,(SELECT commune_name FROM ln_commune WHERE com_id=com_id limit 1) As com_id
+          ,(SELECT village_name FROM ln_village WHERE vill_id=village_id limit 1) As village_id 
+          ,spouse_name,phone FROM ln_client ORDER BY client_id";
           return $db->fetchAll($sql);
-    }
+    } 
     public function getAllGroup(){
     	$db = $this->getAdapter();
     	$sql="SELECT client_number,name_kh,name_en,sex,status,(SELECT branch_namekh FROM ln_branch WHERE br_id =branch_id limit 1) AS branch_name
--- application/modules/report/models/DbTable/DbParamater.php
+++ application/modules/report/models/DbTable/DbParamater.php
@@ -1,10 +1,7 @@
 <?php
 class Report_Model_DbTable_DbParamater extends Zend_Db_Table_Abstract
 {
-      
-     
-
-    public function getAllHoliday(){
+      public function getAllHoliday(){
     	 $db = $this->getAdapter();
           $sql="SELECT id,holiday_name,amount_day,start_date,end_date,STATUS,modify_date,note FROM ln_holiday ORDER BY id";
           return $db->fetchAll($sql);
@@ -16,7 +13,7 @@ class Report_Model_DbTable_DbParamater extends Zend_Db_Table_Abstract
     }
     public function getALLstaff(){
     	$db = $this->getAdapter();
-    	$sql="SELECT co_id,co_khname,co_firstname,sex,(SELECT position_kh FROM ln_position WHERE id=position_id) As position,tel,basic_salary,national_id,address,
+    	$sql="SELECT co_id,co_khname,co_firstname,(SELECT name_kh FROM ln_view WHERE TYPE = 11 AND key_code=sex ) AS sex,(SELECT position_kh FROM ln_position WHERE id=position_id) As position,tel,basic_salary,national_id,address,
     	(SELECT branch_namekh FROM ln_branch WHERE br_id =branch_id limit 1) AS branch_name,note FROM ln_co ORDER BY co_id";
     	return $db->fetchAll($sql);
     }
