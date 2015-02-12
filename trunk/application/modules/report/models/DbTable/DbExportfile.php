a1993b08e167b12824aca1a2949b518cc045a54c
thekhmerkravsrok@gmail.com <thekhmerkravsrok@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2015-02-12 14:08:53 +0000

    update export file excel to server

---
+++ trunk/application/modules/report/models/DbTable/DbExportfile.php
@@ -0,0 +1,21 @@
+<?php
+class Report_Model_DbTable_DbExportfile extends Zend_Db_Table_Abstract
+{
+      public function getFileby($data ,$table){
+		$db = $this->getAdapter();
+      	$db = new Application_Model_DbTable_DbGlobal();
+      	$sql = "SHOW COLUMNS FROM $table ";
+      	$datahead=$db->getGlobalDb($sql);
+      	foreach($datahead as $id =>$rs){
+      		$thead[]=$rs['Field'];
+      	}
+      	foreach ($data  AS $index => $row)
+      	{
+      		foreach($datahead as $id =>$rs){
+      			$finalData[$index][$id]=utf8_decode($row[$rs['Field']]);
+      		}
+      	}
+      	return array('data'=>$finalData,'header_title'=>$thead);
+      }
+ }
+
