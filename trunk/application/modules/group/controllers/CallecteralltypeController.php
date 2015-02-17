5be2dd5c779534cf18d7e7cb68a07526c60f89f2
Kysyheng@gmail.com <Kysyheng@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2015-02-17 09:39:27 +0000

    complete customer

---
+++ trunk/application/modules/group/controllers/CallecteralltypeController.php
@@ -0,0 +1,97 @@
+<?php
+
+class Group_CallecteralltypeController extends Zend_Controller_Action
+{
+
+public function init()
+    {
+        /* Initialize action controller here */
+    	header('content-type: text/html; charset=utf8');
+    	defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
+    }
+
+    public function addAction()
+    {
+    	if($this->getRequest()->isPost()){
+			$data=$this->getRequest()->getPost();
+			$db = new Group_Model_DbTable_DbCallecteralltype();
+			try {
+				//print_r($data);exit();
+				if(isset($data['btn_save'])){
+					$db->addcallecterall($data);
+					Application_Form_FrmMessage::message('ការ​បញ្ចូល​​ជោគ​ជ័យ');
+				}
+				if(isset($data['btn_save_close'])){
+					$db->addcallecterall($data);
+					Application_Form_FrmMessage::message('ការ​បញ្ចូល​​ជោគ​ជ័យ');
+					Application_Form_FrmMessage::redirectUrl('/group/Callecteralltype');
+				}
+			} catch (Exception $e) {
+				Application_Form_FrmMessage::message("INSERT_FAIL");
+				$err = $e->getMessage();
+				
+				Application_Model_DbTable_DbUserLog::writeMessageError($err);
+			}
+		}
+       $fm = new Callecterall_Form_Frmcallecterall();
+	   $frm = $fm->Frmcallecterall(); 
+	   Application_Model_Decorator::removeAllDecorator($frm);
+	   $this->view->Form_Frmcallecterall = $frm;
+	   
+    }
+    public function indexAction()
+    {
+    	try{
+    		$db = new Callecterall_Model_DbTable_DbCallecterall();
+    			
+    		$rs_rows= $db->geteAllid($search=null);//call frome model
+    		$glClass = new Application_Model_GlobalClass();
+    		$rs_rows = $glClass->getImgActive($rs_rows, BASE_URL, true);
+    		$list = new Application_Form_Frmtable();
+    		$collumns = array("NAME_EN","NAME_KH","DATE","STATUS");
+    		$link=array(
+    				'module'=>'group','controller'=>'Callecteralltype','action'=>'edit',
+    		);
+    		$this->view->list=$list->getCheckList(0, $collumns,$rs_rows,array('title_en'=>$link,'title_kh'=>$link));
+    	}catch (Exception $e){
+    		Application_Form_FrmMessage::message("Application Error");
+    		echo $e->getMessage();
+    		Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
+    	}
+    }
+    public function editAction()
+    {
+    if($this->getRequest()->isPost()){
+			$data=$this->getRequest()->getPost();
+			$db = new Group_Model_DbTable_DbCallecteralltype();
+			try {
+				if(isset($data['btn_save'])){
+					$db->updatcallecterall($data);
+					Application_Form_FrmMessage::message('ការ​បញ្ចូល​​ជោគ​ជ័យ');
+				}
+				if(isset($data['btn_save_close'])){
+					$db->updatcallecterall($data);
+					Application_Form_FrmMessage::message('ការ​បញ្ចូល​​ជោគ​ជ័យ');
+					Application_Form_FrmMessage::redirectUrl('/group/Callecteralltype');
+				}				
+			} catch (Exception $e) {
+				Application_Form_FrmMessage::message("INSERT_FAIL");
+				$err = $e->getMessage();
+				
+				Application_Model_DbTable_DbUserLog::writeMessageError($err);
+			}
+		
+    	}
+    	$id = $this->getRequest()->getParam('id');
+    		
+    	$db = new Callecterall_Model_DbTable_DbCallecterall();
+    	$row  = $db->getcallecterallbyid($id);
+    	$fm = new Callecterall_Form_Frmcallecterall();
+	    $frm = $fm->Frmcallecterall($row); 
+	    Application_Model_Decorator::removeAllDecorator($frm);
+	    $this->view->Form_Frmcallecterall = $frm;
+    		
+    
+    }
+}
+?>
---
+++ trunk/application/modules/group/forms/Frmcallecteralltype.php
@@ -0,0 +1,68 @@
+<?php 
+Class Group_Form_Frmcallecteralltype extends Zend_Dojo_Form {
+	protected $tr;
+	public function init()
+	{
+		$this->tr = Application_Form_FrmLanguages::getCurrentlanguage();
+	}
+	public function Frmcallecterall($data=null){
+		
+		$name_en = new Zend_Dojo_Form_Element_TextBox('title_en');
+		$name_en->setAttribs(array(
+				'dojoType'=>'dijit.form.TextBox',
+				'class'=>'fullside',
+				'required'=>true
+		));
+		
+		$name_kh = new Zend_Dojo_Form_Element_TextBox('title_kh');
+		$name_kh->setAttribs(array(
+				'dojoType'=>'dijit.form.TextBox',
+				'class'=>'fullside',
+				'required'=>true
+		));
+		$key_code = new Zend_Dojo_Form_Element_NumberTextBox('key_code');
+		$key_code->setAttribs(array(
+				'dojoType'=>'dijit.form.NumberTextBox',
+				'class'=>'fullside',
+				'required'=>true
+				));
+		
+		$_display=  new Zend_Dojo_Form_Element_FilteringSelect('display_by');
+		$_display->setAttribs(array('dojoType'=>'dijit.form.FilteringSelect','class'=>'fullside',));
+		$_display_opt = array(
+				1=>$this->tr->translate("NAME_KHMER"),
+				2=>$this->tr->translate("NAME_ENGLISH"));
+		$_display->setMultiOptions($_display_opt);
+		
+		$_arr = array(1=>$this->tr->translate("ACTIVE"),0=>$this->tr->translate("DACTIVE"));
+		$_status = new Zend_Dojo_Form_Element_FilteringSelect("status");
+		$_status->setMultiOptions($_arr);
+		$_status->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'required'=>'true',
+				'missingMessage'=>'Invalid Module!',
+				'class'=>'fullside'));
+		$date_call = new Zend_Dojo_Form_Element_DateTextBox('date');
+		$date_call->setAttribs(array(
+				'dojoType'=>'dijit.form.DateTextBox',
+				'class'=>'fullside',
+				'required'=>true
+		));
+		$date_call->setValue(date('Y-m-d'));
+	  $_id = new Zend_Form_Element_Hidden('id');
+		
+		if($data!=null){
+			$name_en->setValue($data['title_en']);
+			$name_kh->setValue($data['title_kh']);
+			$_display->setValue($data['display_by']);
+			$_status->setValue($data['status']);
+			$date_call->setValue($data['date']);
+			//$key_code->setValue($data['key_code']);
+		    $_id->setValue($data['id']);
+			
+		}
+		$this->addElements(array($name_en,$name_kh,$key_code,$_id,$_display,$_status,$date_call));
+		return $this;
+		
+	}	
+}
