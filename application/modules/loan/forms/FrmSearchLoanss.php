6606da43388187e463b01d2a475708e5b3bc6d10
phanetnan <phanetnan@gmail.com>
2015-02-24 10:04:34 +0000

    loanss

---
+++ application/modules/loan/forms/FrmSearchLoanss.php
@@ -0,0 +1,160 @@
+<?php 
+Class Loan_Form_FrmSearchLoanss extends Zend_Dojo_Form {
+	protected $tr;
+	public function init()
+	{
+		$this->tr = Application_Form_FrmLanguages::getCurrentlanguage();
+	}
+	public function AdvanceSearch ($data=null){
+		
+		$db = new Application_Model_DbTable_DbGlobal();
+		
+		$request=Zend_Controller_Front::getInstance()->getRequest();
+		$_status=  new Zend_Dojo_Form_Element_FilteringSelect('status');
+		$_status->setAttribs(array('dojoType'=>'dijit.form.FilteringSelect'));
+		$_status_opt = array(
+				-1=>$this->tr->translate("ALL"),
+				1=>$this->tr->translate("ACTIVE"),
+				0=>$this->tr->translate("DACTIVE"));
+		$_status->setMultiOptions($_status_opt);
+		$_status->setValue($request->getParam("status"));
+		
+		$_title = new Zend_Dojo_Form_Element_TextBox('adv_search');
+		$_title->setAttribs(array('dojoType'=>'dijit.form.TextBox',
+				'onkeyup'=>'this.submit()',
+				'placeholder'=>$this->tr->translate("ADVANCE_SEARCH")
+		));
+		$_title->setValue($request->getParam("adv_search"));
+		
+		$_btn_search = new Zend_Dojo_Form_Element_SubmitButton('btn_search');
+		$_btn_search->setAttribs(array(
+				'dojoType'=>'dijit.form.Button',
+				'iconclass'=>'dijitIconSearch',
+		
+		));
+		
+		$_group_code = new Zend_Dojo_Form_Element_FilteringSelect('group_code');
+		$_group_code->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'class'=>'fullside',
+				'onchange'=>'getmemberIdGroup();'
+		));
+		$group_opt = $db ->getGroupCodeById(1,1,1);
+		$_group_code->setMultiOptions($group_opt);
+		$_group_code->setValue($request->getParam("group_code"));
+		
+		$_customer_code = new Zend_Dojo_Form_Element_FilteringSelect('customer_code');
+		$_customer_code->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'onchange'=>'getmemberIdGroup();'
+		));
+		$group_opt = $db ->getGroupCodeById(1,0,1);//code,individual,option
+		$_customer_code->setMultiOptions($group_opt);
+		$_customer_code->setValue($request->getParam("customer_code"));
+		
+		$_member = new Zend_Dojo_Form_Element_FilteringSelect('member');
+		$_member->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'onchange'=>'checkMember()'
+		));
+		$options = $db->getGroupCodeById(2,0,1);
+		$_member->setMultiOptions($options);
+		$_member->setValue($request->getParam("member"));
+		
+		$_groupid = new Zend_Dojo_Form_Element_FilteringSelect('group_id');
+		$_groupid->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+ 				'onchange'=>'popupCheckClient();'
+				));
+		$options = $db ->getGroupCodeById(2,1,1);//show name,show group,show option
+		$_groupid->setMultiOptions($options);
+		$_groupid->setValue($request->getParam("group_id"));
+		
+		$_coid = new Zend_Dojo_Form_Element_FilteringSelect('co_id');
+		$_coid->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'onchange'=>'popupCheckCO();'
+		));
+		$options = $db ->getAllCOName(1);
+		$_coid->setMultiOptions($options);
+		$_coid->setValue($request->getParam("co_id"));
+		
+		$_currency_type = new Zend_Dojo_Form_Element_FilteringSelect('currency_type');
+		$_currency_type->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+		));
+		$opt = array(2=>"Dollar",1=>'Khmer',3=>"Bath");
+		$_currency_type->setMultiOptions($opt);
+		$_currency_type->setValue($request->getParam("currency_type"));
+		
+		$_repayment_method = new Zend_Dojo_Form_Element_FilteringSelect('repayment_method');
+		$_repayment_method->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'required' =>'true',
+				'onchange'=>'chechPaymentMethod()'
+		));
+		$options = $db->getAllPaymentMethod(null,1);
+		$_repayment_method->setMultiOptions($options);
+		$_repayment_method->setValue($request->getParam("repayment_method"));
+		
+		$_zone = new Zend_Dojo_Form_Element_FilteringSelect('zone');
+		$_zone->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'onchange'=>'popupCheckZone();'
+		));
+		$options = $db ->getZoneList(1);
+		$_zone->setMultiOptions($options);
+		$_zone->setValue($request->getParam("zone"));
+		
+		$_releasedate = new Zend_Dojo_Form_Element_DateTextBox('from_date');
+		$_releasedate->setAttribs(array(
+				'dojoType'=>'dijit.form.DateTextBox',
+		));
+		$s_date = date('Y-m-d');
+		$_releasedate->setValue($s_date);
+		
+		
+		$_dateline = new Zend_Dojo_Form_Element_DateTextBox('to_date');
+		$_dateline->setAttribs(array(
+				'dojoType'=>'dijit.form.DateTextBox',
+		));
+		$_dateline->setValue($s_date);
+		
+		$_payterm = new Zend_Dojo_Form_Element_FilteringSelect('payment_term');
+		$_payterm->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+				'class'=>'fullside',
+				'required' =>'true'
+		));
+		$options= array(1=>"Day",2=>"Week",3=>"Month");
+		$_payterm->setMultiOptions($options);
+		$_payterm->setValue($request->getParam("payment_term"));
+		
+		$_branch_id = new Zend_Dojo_Form_Element_FilteringSelect('branch_id');
+		$_branch_id->setAttribs(array(
+				'dojoType'=>'dijit.form.FilteringSelect',
+		));
+		
+		$rows = $db->getAllBranchName();
+		$options='';
+			if(!empty($rows))foreach($rows AS $row){
+				$options[$row['br_id']]=$row['branch_namekh'];
+			}
+		$_branch_id->setMultiOptions($options);
+		$_branch_id->setValue($request->getParam("branch_id"));
+		
+		if($data!=null){
+			print_r($data);
+			$_branch_id->setValue($data['member_id']);
+			$_member->setValue($data['client_id']);
+			$_coid->setValue($data['co_id']);
+			$_zone->setValue($data['zone_id']);
+			$_releasedate->setValue($data['date_release']);
+			$_currency_type->setValue($data['payment_method']);
+		}
+		$this->addElements(array($_groupid,$_title,$_branch_id,$_member,$_coid,$_currency_type,$_zone,$_releasedate
+				,$_payterm,$_dateline,$_group_code,$_customer_code,$_status,$_btn_search,$_repayment_method));
+		return $this;
+		
+	}	
+}
---
+++ application/modules/loan/models/DbTable/DbLoanss.php
@@ -0,0 +1,155 @@
+<?php
+
+class Loan_Model_DbTable_DbLoanss extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'ln_loan_group';
+    public function getUserId(){
+    	$session_user=new Zend_Session_Namespace('auth');
+    	return $session_user->user_id;
+    	 
+    }
+    public function addNewLoanGroup($data){
+    	
+    	$db = $this->getAdapter();
+    	$db->beginTransaction();
+    	try{
+    		$datagroup = array(
+    				'group_id'=>$data['group_id'],
+    				'co_id'=>$data['co_id'],
+    				'zone_id'=>$data['zone'],
+    				'level'=>1,//$data[''],
+    				'date_release'=>$data['release_date'],
+    				'total_duration'=>$data['period'],
+    				'first_payment'=>$data['first_payment'],
+    				'time_collect'=>$data['time'],
+    				'pay_term'=>$data['pay_every'],
+    				'payment_method'=>$data['repayment_method'],
+    				'holiday'=>$data['every_payamount'],
+    				'is_renew'=>0,
+    				);
+    		$g_id = $this->insert($datagroup);//add group loan
+    		$dbtable = new Application_Model_DbTable_DbGlobal();
+    		$tranlist = Zend_Json::decode($data['tran_list']);
+    		foreach ($tranlist as $key => $row) {
+    			$datamember = array(
+    					'group_id'=>$g_id,
+    					'client_id'=>$row['member_id'],
+//     					'payment_method'=>$data['payment_method'],
+    					'currency_type'=>$row['currency_type_id'],
+    					'total_capital'=>$row['total_amount'],//$data[''],
+    					'interest_rate'=>$data['interest_rate'],
+    					'status'=>1,
+    					'is_completed'=>0,
+    					'branch_id'=>1
+    			);
+    			$this->_name='ln_loan_member';
+    			$g_id = $this->insert($datamember);//add member loan
+    			
+    			$remain_principal = $row['total_amount'];
+    			$next_month = $data['first_payment'];
+    			$start_date = $data['release_date'];//loan release;
+    			
+    			$this->_name='ln_loanmember_funddetail';
+    			for($i=1;$i<=$data['period'];$i++){
+    				
+    				$payment_method = $data['repayment_method'];
+    				if($payment_method==1){
+    					$pri_permonth = $row['total_amount']/$data['period'];
+    					if($i!=1){
+    						$remain_principal = $remain_principal-$pri_permonth;//OSប្រាក់ដើមគ្រា
+    					}
+    					
+    				}elseif($payment_method==2){
+    					$pri_permonth=0;
+    					if($i==$data['period']){
+    						$pri_permonth = $row['total_amount'];
+    						$remain_principal = $pri_permonth;//$remain_principal-$pri_permonth;//OSប្រាក់ដើមគ្រា
+    					}
+    				}elseif($payment_method==3){
+    					
+    				}elseif($payment_method==4){
+    					
+    				}elseif($payment_method==5){
+    					
+    				}elseif($payment_method==6){
+    					
+    				}
+    				
+    				$day_perterm = $this->getSubDaysByPaymentTerm($data['pay_every']);//return amount day for payterm
+    				if($i!=1){
+//     					$remain_principal = $remain_principal-$pri_permonth;//OSប្រាក់ដើមគ្រា
+    					$start_date = $next_month;
+    					$d = new DateTime($next_month);
+    					
+    					$str_next = $this->getNextDateById($data['pay_every']);//for next,day,week,month;
+    					
+    					$d->modify($str_next);
+    					$next_month_before =  $d->format( 'Y-m-d' );
+    					$next_month = $dbtable->checkHolidayExist($str_next,$next_month_before);//copare if next month above not the same below
+						
+						$amount_day = $this->CountDayByDate($start_date,$next_month);
+						$interest_paymonth = $remain_principal*($data['interest_rate']/100)*($amount_day/$day_perterm);
+    					
+    				}else{
+    					$amount_day = $this->CountDayByDate($start_date,$next_month);
+//     					return $amount_day;
+    					$interest_paymonth = $row['total_amount']*($data['interest_rate']/100)*($amount_day/$day_perterm);
+    					
+    				}
+    				
+    				
+    				
+    				$datapayment = array(
+    						'member_id'=>$g_id,
+    						'total_principal'=>$remain_principal,
+    						'principal_permonth'=> $pri_permonth,
+    						'total_interest'=>$interest_paymonth,
+    						'total_payment'=>$pri_permonth+$interest_paymonth,
+    						'date_payment'=>$next_month,
+    						'is_completed'=>0,
+    						'branch_id'=>1,
+    						'status'=>1,
+    						'amount_day'=>$amount_day,
+    				);
+    				$this->insert($datapayment);
+    			}
+    		}
+    		$db->commit();
+    		return 1;
+    	}catch (Exception $e){
+    		$db->rollBack();
+    		return $e->getMessage();
+    	}
+    }
+    public function getNextDateById($pay_term){
+    	if($pay_term==3){
+    		$str_next = 'next month';
+    	}elseif($pay_term==2){
+    		$str_next = 'next week';
+    	}else{
+    		$str_next = 'next day';
+    	}
+    	return $str_next;
+    }
+    public function getSubDaysByPaymentTerm($pay_term){
+    	if($pay_term==3){
+    		$amount_days =30;
+    	}elseif($pay_term==2){
+    		$amount_days =7;
+    	}else{
+    		$amount_days =1;
+    	}
+    	return $amount_days;
+    	
+    }
+    public function CountDayByDate($start,$end){
+    	$db = new Application_Model_DbTable_DbGlobal();
+    	return ($db->countDaysByDate($start,$end));
+    }
+    public function CalculateByMethod($method_type){
+    	
+    }
+  
+}
+
