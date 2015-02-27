5f3fb75898ac173fe48fec56f08ee7eb8e846e0f
thekhmerkravsrok <thekhmerkravsrok@gmail.com>
2015-02-27 07:54:57 +0000

    add setting

---
+++ application/modules/setting/models/DbTable/DbRestore.php
@@ -0,0 +1,29 @@
+<?php
+class Setting_Model_DbTable_DbRestore extends Zend_Db_Table_Abstract
+{
+	public function getRestoreDatabase($file_name){
+		$db = $this->getAdapter();
+		$part =  PUBLIC_PATH;
+		$str =  file_get_contents($part.'/'.$file_name);
+		$strresult=$db->query($str);
+		unlink($part.'/'.$file_name);
+		// 			return $r;
+	}
+	
+	public function UploadFileDatabase($data){
+		$adapter = new Zend_File_Transfer_Adapter_Http();
+		$adapter->setDestination(PUBLIC_PATH);
+		$fileinfo=$adapter->getFileInfo();
+		$rs = $adapter->receive();
+		if($rs==1){
+// 			return true;
+			$file_name = $fileinfo['fileToUpload']['name'];
+			$this->getRestoreDatabase($file_name);
+		}else{
+			return false;
+		}
+	}   
+}
+
+
+
