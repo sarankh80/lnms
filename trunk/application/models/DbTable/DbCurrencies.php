50406387c1f73167c7296f5553a953dd6e50656e
phanetnan@gmail.com <phanetnan@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2015-03-06 09:44:38 +0000

    xchanges 6 3 2015

--- trunk/application/models/DbTable/DbCurrencies.php
+++ trunk/application/models/DbTable/DbCurrencies.php
@@ -7,8 +7,8 @@ class Application_Model_DbTable_DbCurrencies extends Zend_Db_Table_Abstract
 
 	function getCurrencyList(){
 		$db = $this->getAdapter();
-		$sql = "SELECT id, name, symbol
-				FROM `cs_currencies`";		
+		$sql = "SELECT id, curr_nameen as name, symbol
+				FROM `ln_currency`";		
 		return $db->fetchAll($sql);
 	}
 }
