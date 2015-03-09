4b04d22ff77940e5e80e5feef0f184aa126f8e41
sovon.khom <sovon.khom@gmail.com>
2015-03-09 08:58:31 +0000

    update parameter to server

--- application/modules/other/controllers/HolidayController.php
+++ application/modules/other/controllers/HolidayController.php
@@ -1,8 +1,11 @@
 <?php
 class Other_HolidayController extends Zend_Controller_Action {
+	protected $tr;
+	const REDIRECT_URL ='/other';
 	public function init()
 	{
 		/* Initialize action controller here */
+		$this->tr=Application_Form_FrmLanguages::getCurrentlanguage();
 		header('content-type: text/html; charset=utf8');
 		defined('BASE_URL')	|| define('BASE_URL', Zend_Controller_Front::getInstance()->getBaseUrl());
 	}
@@ -15,27 +18,27 @@ class Other_HolidayController extends Zend_Controller_Action {
 			else{
 				$search = array(
 						'adv_search' => '',
-						'status' => -1);
+						'search_status' => -1);
 			}
 			$rs_rows= $db->getAllHoliday($search);
 			$glClass = new Application_Model_GlobalClass();
 			$rs_rows = $glClass->getImgActive($rs_rows, BASE_URL, true);
 			$list = new Application_Form_Frmtable();
-			$collumns = array("HOLIDAY_NAME","AMOUNT","START_DATE","END_DATE","STATUS","BY");
+			$collumns = array("HOLIDAY_NAME","AMOUNT","START_DATE","END_DATE","NOTE","STATUS","BY");
 			$link=array(
 					'module'=>'other','controller'=>'Holiday','action'=>'edit',
 			);
-			$this->view->list=$list->getCheckList(0, $collumns, $rs_rows,array('holiday_name'=>$link));
+			$this->view->list=$list->getCheckList(0, $collumns, $rs_rows,array('holiday_name'=>$link,'start_date'=>$link,'amount_day'=>$link));
 		}catch (Exception $e){
 			Application_Form_FrmMessage::message("Application Error");
 			echo $e->getMessage();
 			Application_Model_DbTable_DbUserLog::writeMessageError($e->getMessage());
 		}
 	
-		$frm = new Application_Form_FrmAdvanceSearch();
-		$frm = $frm->AdvanceSearch();
+		$frm = new Other_Form_FrmHoliday();
+		$frm = $frm->FrmAddHoliday();
 		Application_Model_Decorator::removeAllDecorator($frm);
-		$this->view->frm_search = $frm;
+		$this->view->frm_holiday = $frm;
 	}
 	function addAction()
 	{
@@ -44,9 +47,13 @@ class Other_HolidayController extends Zend_Controller_Action {
 			try {
 				$db = new Other_Model_DbTable_DbHoliday();
 				$_major_id = $db->addHoliday($_data);
-				Application_Form_FrmMessage::message("ការ​បញ្ចូល​ជោគ​ជ័យ !");
+				if(!empty($_data['save_new'])){
+					Application_Form_FrmMessage::message($this->tr->translate('INSERT_SUCCESS'));
+				}else{
+					Application_Form_FrmMessage::Sucessfull($this->tr->translate('INSERT_SUCCESS'), self::REDIRECT_URL . '/Holiday/index');
+				}
 			} catch (Exception $e) {
-				Application_Form_FrmMessage::message("ការ​បញ្ចូល​មិន​ជោគ​ជ័យ");
+				Application_Form_FrmMessage::message($this->tr->translate('INSERT_FAIL'));
 				$err =$e->getMessage();
 				Application_Model_DbTable_DbUserLog::writeMessageError($err);
 			}
