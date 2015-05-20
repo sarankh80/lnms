985118044daaedcf8179a64b865ec125e268c9a5
kekimsoeurn099@gmail.com <kekimsoeurn099@gmail.com@fd4f8545-de5e-3209-35c7-272b4404d2a5>
2015-05-20 09:10:35 +0000

    new update rpt-loan-collect

--- trunk/application/modules/report/models/DbTable/DbloanCollect.php
+++ trunk/application/modules/report/models/DbTable/DbloanCollect.php
@@ -9,40 +9,37 @@ class Report_Model_DbTable_DbloanCollect extends Zend_Db_Table_Abstract
 //     }
     public function getAllLnClient($search=null){
     	$db=$this->getAdapter();
-    	$from_date =(empty($search['from_date']))? '1': "date_payment < '".$search['from_date']." 00:00:00'";
-    	$to_date = (empty($search['to_date']))? '1': "date_payment <= '".$search['to_date']." 23:59:59'";
-    	$where = " AND ".$from_date." OR ".$to_date;
-    	$sql = "SELECT f.id,f.`member_id`,f.`total_principal`,f.principal_permonth,f.total_interest,f.total_payment,f.amount_day,f.date_payment,
-				(SELECT  co_khname FROM ln_co WHERE co_id=f.`collect_by` ) AS co_name,
-				(SELECT branch_namekh FROM ln_branch WHERE br_id=f.branch_id) AS branch_kh,
-				(SELECT CONCAT (client_number,' - ',name_kh,'-',name_en,' <br /> ',phone,'-','st:',street,';No:',house)  FROM ln_client WHERE client_id= (SELECT client_id FROM ln_loan_member WHERE member_id=f.member_id limit 1) limit 1)
-				AS client_name FROM `ln_loanmember_funddetail` AS f WHERE is_completed=0 AND status=1 ";
-    	if(!empty($search['client_code'])){
+    	$start_date = $search['start_date'];
+   		$end_date = $search['end_date'];
+    	$sql = "SELECT * FROM v_getloancollects Where 1";
+    	$where ='';
+    	if(!empty($search['start_date']) or !empty($search['end_date'])){
+    		$where.=" AND date_payment BETWEEN '$start_date' AND '$end_date'";
+    	}
+    	if($search['branch_id']>0){
+    		$where.=" AND branch_id = ".$search['branch_id'];
+    	}
+    	if($search['client_name']>0){
+    		$where.=" AND client_id = ".$search['client_name'];
+    	}
+    	if($search['co_id']>0){
+    		$where.=" AND co_id = ".$search['co_id'];
+    	}
+    	if(!empty($search['adv_search'])){
     		$s_where = array();
-    		$s_search = $search['btn_search'];
-    		$s_where[] = "name_kh,name_en LIKE '%{$s_search}%'";
-    		$s_where[] = " co_khname LIKE '%{$s_search}%'";
-    		$s_where[] = " branch_namekh LIKE '%{$s_search}%'";
-    		    		$s_where[] = " f.total_principal LIKE '%{$s_search}%'";
-    		    		$s_where[] = " f.principal_permonth LIKE '%{$s_search}%'";
-    		    		$s_where[] = " f.total_interest LIKE '%{$s_search}%'";
-    		    		$s_where[] = " f.amount_day LIKE '%{$s_search}%'";
-    		    		$s_where[] = " f.date_payment LIKE '%{$s_search}%'";
+    		$s_search = $search['adv_search'];
+    		$s_where[] = "branch_kh LIKE '%{$s_search}%'";
+    		$s_where[] = " co_name LIKE '%{$s_search}%'";
+    		$s_where[] = " client_name LIKE '%{$s_search}%'";
+    		$s_where[] = " total_principal LIKE '%{$s_search}%'";
+    		$s_where[] = " principal_permonth LIKE '%{$s_search}%'";
+    		$s_where[] = " total_interest LIKE '%{$s_search}%'";
+    		$s_where[] = " amount_day LIKE '%{$s_search}%'";
     		$where .=' AND '.implode(' OR ',$s_where).'';
     	}
-//     	if($search['status']>0){
-//     		$where.= " AND status = ".$search['status'];
-//     	}
-    	//     	if($search['province']>0){
-    	//     		$where.=" AND pro_id= ".$search['province'];
-    	//     	}
-//     	if($search['branch_id']>0){
-//     		$where.=" AND branch_id= ".$search['branch_id'];
-//     	}
-    	//$order=" ORDER BY client_id DESC";
     	//echo $sql.$where;
     	return $db->fetchAll($sql.$where);
-    	//return $db->fetchAll($sql);
+    	
     }
 
 	
